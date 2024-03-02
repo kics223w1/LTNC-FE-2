6e3edebdc3a15ed4e7f29d823f1aacc9720d2bcb
Huy Cao <77295189+kics223w1@users.noreply.github.com>
2024-03-03 00:58:28 +0700

    refactor: add table (#1)

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+import '@/lib/env';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
+const rows: GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  { id: 4, col1: 'MUI', col2: 'is Amazing' },
+  { id: 5, col1: 'MUI', col2: 'is Amazing' },
+  { id: 6, col1: 'MUI', col2: 'is Amazing' },
+  { id: 7, col1: 'MUI', col2: 'is Amazing' },
+  { id: 8, col1: 'MUI', col2: 'is Amazing' },
+  { id: 9, col1: 'MUI', col2: 'is Amazing' },
+  { id: 10, col1: 'MUI', col2: 'is Amazing' },
+  { id: 11, col1: 'MUI', col2: 'is Amazing' },
+  { id: 12, col1: 'MUI', col2: 'is Amazing' },
+  { id: 16, col1: 'MUI', col2: 'is Amazing' },
+  { id: 14, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
+
+export default function Dashboard() {
+  return (
+    <main>
+      <div className='flex items-center w-screen h-screen'>
+        <div className='w-96 h-full border-r'></div>
+        <div className='flex flex-col justify-between w-full h-full'>
+          <div className='h-12 w-full border-b'></div>
+          <DataGrid className='mx-5 my-5 p-5' rows={rows} columns={columns} />
+          <div className='h-12 w-full border-t'></div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
 'use client';
-
-import Head from 'next/head';
 import '@/lib/env';
-
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
 import Header from '@/app/components/header';
 import SignInForm from '@/app/components/sign-in-form';
 import { useState } from 'react';
 import SignUpForm from '@/app/components/sign-up-form';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
   const [isSignInFormVisible, setIsSignInFormVisible] = useState(true);
